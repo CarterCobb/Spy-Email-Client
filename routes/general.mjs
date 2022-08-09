65d02c3e317433beb24fa8f5af378a12aa2288e0
Carter Cobb <carter.cobb72@gmail.com>
2022-08-09 09:17:55 -0600

    fixed login bug

--- routes/general.mjs
+++ routes/general.mjs
@@ -20,25 +20,28 @@ export default [
           return res.status(400).json({ message: "Missing credentials" });
         req.session.EMAIL_USERNAME = req.body.username;
         req.session.EMAIL_PASSWORD = req.body.password;
-        req.session.save();
-        const user_id = req.session.EMAIL_USERNAME.split("@")[0].toLowerCase();
-        if (fs.existsSync(`./keys/${user_id}_private.pem`))
-          return res.sendStatus(200);
-        child_process.exec(
-          `openssl genrsa -aes256 -out ./keys/${user_id}_private.pem -passout pass:${req.session.EMAIL_PASSWORD} 2048`,
-          (err) => {
-            if (err) return res.status(500).json({ message: err.message });
-            else
-              child_process.exec(
-                `openssl rsa -in ./keys/${user_id}_private.pem -passin pass:${req.session.EMAIL_PASSWORD} -pubout > ./keys/${user_id}_public.pem`,
-                (err2) => {
-                  if (err2)
-                    return res.status(500).json({ message: err2.message });
-                  return res.sendStatus(200);
-                }
-              );
-          }
-        );
+        req.session.save((err) => {
+          if (err) return res.status(500).json({ message: err.message });
+          const user_id =
+            req.session.EMAIL_USERNAME.split("@")[0].toLowerCase();
+          if (fs.existsSync(`./keys/${user_id}_private.pem`))
+            return res.sendStatus(200);
+          child_process.exec(
+            `openssl genrsa -aes256 -out ./keys/${user_id}_private.pem -passout pass:${req.session.EMAIL_PASSWORD} 2048`,
+            (err) => {
+              if (err) return res.status(500).json({ message: err.message });
+              else
+                child_process.exec(
+                  `openssl rsa -in ./keys/${user_id}_private.pem -passin pass:${req.session.EMAIL_PASSWORD} -pubout > ./keys/${user_id}_public.pem`,
+                  (err2) => {
+                    if (err2)
+                      return res.status(500).json({ message: err2.message });
+                    return res.sendStatus(200);
+                  }
+                );
+            }
+          );
+        });
       },
     ],
   },
