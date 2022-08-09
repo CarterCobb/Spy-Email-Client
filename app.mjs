3d2ebbb888ded563d75f7a8e5f5080cdd5c27f76
Carter Cobb <carter.cobb72@gmail.com>
2022-08-09 11:17:50 -0600

    removed node cluster

--- app.mjs
+++ app.mjs
@@ -1,17 +1,12 @@
 import express from "express";
-import cluster from "cluster";
-import os from "os";
+
 import * as NSR from "node-server-router";
 import Session from "./session.mjs";
 
-if (cluster.isPrimary)
-  for (var _ = 0; _ < os.cpus().length; _++) cluster.fork();
-else {
-  const app = express();
-  app.use(express.json({ limit: "50mb" }));
-  NSR.RouteFactory.applyRoutesTo(app, { api_version: "/v1" });
-  Session.configureOn(app);
-  app.listen(6969, () =>
-    console.log(`[pid:${process.pid}] Listening on port: 6969.`)
-  );
-}
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+NSR.RouteFactory.applyRoutesTo(app, { api_version: "/v1" });
+Session.configureOn(app);
+app.listen(6969, () =>
+  console.log(`[pid:${process.pid}] Listening on port: 6969.`)
+);
