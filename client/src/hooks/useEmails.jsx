96d106d4a54efd45195fa2597a9e7cc8b7c42c87
Carter Cobb <carter.cobb72@gmail.com>
2022-08-09 10:20:04 -0600

    fixed email hook

--- client/src/hooks/useEmails.jsx
+++ client/src/hooks/useEmails.jsx
@@ -1,32 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 /**
  * Get emails from the API safely.
- * @returns {{list: Array<Object>, error: Error, loading: Boolean, request: Function}}
+ * @returns {{list: Array<Object>, error: Error, loading: Boolean}}
  */
 const useEmails = () => {
   const [emails, setEmails] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  /**
-   * Request the full email objects
-   */
-  const request = () => {
-    setLoading(true);
-    axios
-      .get("/v1/receive")
-      .then(({ data }) => setEmails(data))
-      .catch(setError)
-      .finally(() => setLoading(false));
-  };
+  useEffect(() => {
+    var mounted = true;
+    if (mounted) {
+      setLoading(true);
+      axios
+        .get("/v1/receive")
+        .then(({ data }) => setEmails(data))
+        .catch(setError)
+        .finally(() => setLoading(false));
+    }
+    return () => (mounted = false);
+  }, []);
 
   return {
     list: emails || [],
     error,
     loading,
-    request,
   };
 };
 
