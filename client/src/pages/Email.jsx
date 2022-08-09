b6b79ccc6f3a4860ff6b2b8b472f6e222aa8745b
Carter Cobb <carter.cobb72@gmail.com>
2022-08-09 10:20:16 -0600

    added email hook

--- client/src/pages/Email.jsx
+++ client/src/pages/Email.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { fallback } from "../components/SuspenseUI";
+import useEmails from "../hooks/useEmails";
 
 const Home = () => {
+  const emails = useEmails();
+
+  if (emails.loading) return fallback;
+  if (emails.error) return <pre>{JSON.stringify(emails.error, null, 2)}</pre>;
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Email Page</h1>
+      <p>
+        Emails:
+        <br />
+      </p>
+      <pre>{JSON.stringify(emails.list, null, 2)}</pre>
     </div>
   );
 };
