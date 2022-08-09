fb1de3866e4635220cfb000c38d5e1b63a5c6b7e
Carter Cobb <carter.cobb72@gmail.com>
2022-08-09 10:00:21 -0600

    added `useEmails` hook to query emails

---
+++ client/src/hooks/useEmails.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+
+/**
+ * Get emails from the API safely.
+ * @returns {{list: Array<Object>, error: Error, loading: Boolean, request: Function}}
+ */
+const useEmails = () => {
+  const [emails, setEmails] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  /**
+   * Request the full email objects
+   * @param {Array<String>} ids
+   */
+  const request = () => {
+    setLoading(true);
+    axios
+      .get("/v1/receive")
+      .then(({ data }) => setEmails(data))
+      .catch(setError)
+      .finally(() => setLoading(false));
+  };
+
+  return {
+    list: emails || [],
+    error,
+    loading,
+    request,
+  };
+};
+
+export default useEmails;
