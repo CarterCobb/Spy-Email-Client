2be87de9958a75d58fc415e1e173c82b460e655d
Carter Cobb <carter.cobb72@gmail.com>
2022-08-09 20:47:06 -0600

    formatting

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "./styles/index.css"
-import 'antd/dist/antd.css'
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import "antd/dist/antd.min.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
